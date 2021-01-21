19ea42fbf7647ccc10477550066b785e75a18215
Gabriel Rufino <email@gabrielrufino.com>
2021-01-21 13:32:33 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -282,7 +282,6 @@ describe('Não utilize filtros repetidos', () => {
       return child.innerHTML;
     });
     expect(foundColumnFilter).toEqual(expect.arrayContaining(['orbital_period', 'diameter', 'rotation_period', 'surface_water']));
-    expect(foundColumnFilter).toHaveLength(4);
   });
 });
 
